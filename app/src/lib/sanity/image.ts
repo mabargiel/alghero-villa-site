455d14355b27a63cf318d84a7fd2d41374d5f70d
mbargiel <mbargiel@btopservice.net>
2026-02-07 18:12:18 +0100

    Fix build

--- app/src/lib/sanity/image.ts
+++ app/src/lib/sanity/image.ts
@@ -1,8 +1,8 @@
-import imageUrlBuilder from '@sanity/image-url'
-import {sanityClient} from './client'
+import imageUrlBuilder from "@sanity/image-url";
+import {sanityClient} from "./client";
 
-const builder = imageUrlBuilder(sanityClient)
+const builder = imageUrlBuilder(sanityClient);
 
-export function urlFor(source: unknown) {
-  return builder.image(source)
+export function urlFor(source: Parameters<typeof builder.image>[0]) {
+  return builder.image(source);
 }
