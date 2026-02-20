169537599a36c5a79bbd74f264a5657dde5f1a0d
mbargiel <mbargiel@btopservice.net>
2026-02-20 22:10:08 +0100

    Fix package lock file

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -4,5 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: [String.raw`/((?!api|_next|_vercel|.*\..*).*)`],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
